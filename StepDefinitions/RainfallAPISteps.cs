1accfe8d389a5acc92f5994ed87f570acaa8dc38
EricksonTest <imanzi15sept06@yahoo.co.uk>
2024-02-21 01:06:04 +0000

    Interaction With API

--- StepDefinitions/RainfallAPISteps.cs
+++ StepDefinitions/RainfallAPISteps.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
+using RainfallAPITesting.Drivers;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
 
-namespace Rainfall_API_Official.StepDefinitions
+namespace RainfallAPITesting.StepDefinitions
 {
-    internal class Class1
+
+    [Binding]
+    public class RainfallAPISteps
+
+    private APIDriver apiDriver;
+    private Table responseTable;
+
+    public RainfallAPISteps(APIDriver apiDriver)
     {
+        this.apiDriver = apiDriver;
     }
+}
+
+[Given(@"a measurement station ID ""(.*)""")]
+public void GivenAMeasurementStationID(string StationID)
+
+{
+    APIDriver.Initialize(StationID);
+}
+
+[When(@"I request rainfall measurements with limit ""(.*)""")]
+
+public void WhenIRequestRainfallMeasurementsWithLimit (int limit)
+
+{
+    responseTable = APIDriver.GetRainfallMeasurements(limit);
+}
+
+[Then(@"the response should contain at most (.*) rainfall measurements")]
+public void ThenTheResponseShouldContainAtMostRainfallMeasurements (int expectedCount)
+{
+    Assert.LessOrEqual(responseTable.RowCount, expectedCount);
+}
 }
